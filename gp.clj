9b49d6b994671e39117318d601c691aca1d2e6a6
Larry Diehl <larry@laptoptheliquid.(none)>
2009-03-19 16:16:20 -0400

    Close to replace-node

--- gp.clj
+++ gp.clj
@@ -1,10 +1,12 @@
 (ns gp)
+;;; TODO: Height limit for crossover
 
 (defn- rand-elem [coll]
   (nth coll (rand-int (count coll))))
 
+(defstruct program :value :children)
 (defn p [value & children]
-  {:value value :children children})
+  (struct program value children))
 
 (defn initialize [functions terminals height]
   (if (= 0 height)
@@ -25,16 +27,58 @@
 	   (map #(inc (height %)) children))
     0))
 
+;; (defn replace-node
+;;   ([n replacement tree] (replace-node n 0 replacement tree))
+;;   ([target index replacement tree]
+;;      (if (= target index)
+;;        replacement
+;;        (if-let [children (:children tree)]
+;; 	 (apply p (:value tree)
+;; 		(map #(replace-node 
+;; 		       target (inc index)
+;; 		       replacement %)
+;; 		     children))
+;; 	 tree))))
+
 (defn nth-node
   ([n tree] (nth-node n 0 tree))
   ([target index tree]
-     (if (= target index) tree
+     (if (= target index) 
+       tree
        (if-let [children (:children tree)] 
 	 (reduce #(if (integer? %1)
 		    (nth-node target (inc %1) %2)
 		     %1)
 		 index children)
 	 index))))
+
+;; (defn replace-node
+;;   ([n replacement tree] (first (replace-node n 0 replacement tree)))
+;;   ([target index replacement tree]
+;;      (if (= target index)
+;;        [replacement index]
+;;        (if-let [children (:children tree)]
+;; 	 (apply p (:value tree)
+;; 	   (first (reduce
+;; 	     (fn [[acc i] child]
+;; 	       (conj acc (replace-node target (inc i) replacement child)))
+;; 	     [(vector) index] children)))
+;; 	 [tree index]))))
+
+;;; TODO: rename to set-node
+(defn replace-node
+  ([n replacement tree] (replace-node n 0 replacement tree))
+  ([target index replacement tree]
+     (if (= target index)
+       replacement
+       (if-let [children (:children tree)]
+	 (apply p (:value tree)
+	   (first (reduce
+	     (fn [[acc i] child]
+	       [(conj acc (replace-node target (inc i) replacement child))
+		(inc i)])
+	     [(vector) index] children)))
+	 tree))))
 
 (defn to-sexp [tree]
   (if-let [children (:children tree)]
